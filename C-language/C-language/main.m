228930445309aeb1ffbfa037aa039aeb4f24a6da
wsstssw <o287200812@sohu.com>
2016-11-20 13:07:48 +0800

    创建项目c-language算法

---
+++ C-language/C-language/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  C-language
+//
+//  Created by 赵瑞玮 on 16/11/20.
+//  Copyright © 2016年 zhaoruiwei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
